d39530f55034b44fae419d08739e234561acbb3b
firenzy <lf.oripaypay@gmail.com>
2018-04-10 21:41:43 +0800

    All stored procs now in WCF. Web.config not yet configured

--- WCF/App_Code/IService.cs
+++ WCF/App_Code/IService.cs
@@ -31,5 +31,68 @@ public interface IService
     void KFspFinalizeEvent(string eventid);
     [OperationContract]
     void KFspPopulateScores(string confname, string conlname, string judgefname, string judgelname, string judgeuname, string judgepass, int eventid, string critname);
-
+    [OperationContract]
+    void KFspRemoveContsestantFromEvent(string fname, string lname, string eventid);
+    [OperationContract]
+    void KFspUpdateContestant(string fname, string lname, int perid);
+    [OperationContract]
+    DataTable KFspViewEventContestants(int eventid);
+    [OperationContract]
+    DataTable KFspViewNotEventContestants(int eventid);
+    [OperationContract]
+    DataTable MCspGetEventCriteriaID(string Name, int EventJudgeID);
+    [OperationContract]
+    void MCspUpdateScore(int EventJudgeID, int ContestantID, int EventCriteriaID, float Score);
+    [OperationContract]
+    DataTable MCspViewContestant(int ContestantID);
+    [OperationContract]
+    DataTable MCspViewContestants(string EventName);
+    [OperationContract]
+    DataTable MCspViewContestantsEvent(int EventID);
+    [OperationContract]
+    DataTable MCspViewCriteria(int EventJudgeID);
+    [OperationContract]
+    DataTable MCspViewJudgeEvent(string EventName, int JID);
+    [OperationContract]
+    DataTable MCspViewJudges(int JudgeID);
+    [OperationContract]
+    DataTable MCspViewOfficialResults(int EventID);
+    [OperationContract]
+    DataTable MCspViewScore(int EventJudgeID, int ContestantID, int EventCriteriaID);
+    [OperationContract]
+    DataTable MCspViewScoreWeight(int ContestantID, int EventJudgeID);
+    [OperationContract]
+    DataTable MCspViewStatus(string EventName, int JudgeID, int ContestantID);
+    [OperationContract]
+    void spAddCriteriaToEventCriteria(string cname, int weight, int eventid);
+    [OperationContract]
+    void spAddJudgeToEventJudges(string fname, string lname, string uname, string pass, int eventid);
+    [OperationContract]
+    void spAddOldCriteriaToEventCriteria(string cname, int weight, int eventid);
+    [OperationContract]
+    void spAddPersonToEventJudges(string fname, string lname, string uname, string pass, int eventid);
+    [OperationContract]
+    DataTable spCheckCExistance(string cname, int eventid);
+    [OperationContract]
+    DataTable spCheckcnameavailability(string cname);
+    [OperationContract]
+    DataTable spCheckPersonExistanceinEvent(string fname, string lname, string uname, string pass, int eventid);
+    [OperationContract]
+    DataTable spCheckUnameavailability(string judgechars);
+    [OperationContract]
+    void spRemoveCriteriaFromEventCriteria(string cname, int eventid);
+    [OperationContract]
+    void spRemoveJudgefromEventJudges(string fname, string lname, string uname, string pass);
+    [OperationContract]
+    void spUpdateEventCriteria(string cname, int weight, int eventid, int critid);
+    [OperationContract]
+    void spUpdatePersonJudgeDetails(int personid, string fname, string lname, string uname, string pass, int eventid);
+    [OperationContract]
+    DataTable spViewEventCriteria(int eventid);
+    [OperationContract]
+    DataTable spViewEventJudges(int eventid);
+    [OperationContract]
+    DataTable spViewNotEventCriteria(int eventid);
+    [OperationContract]
+    DataTable spViewNotEventJudges(int eventid);
 }
--- WCF/App_Code/Service.cs
+++ WCF/App_Code/Service.cs
@@ -93,4 +93,218 @@ public class Service : IService
         hasError = databaseCon.HasError;
         error = databaseCon.Error;
     }
+    public void KFspRemoveContsestantFromEvent(string fname, string lname, string eventid)
+    {
+        databaseCon.ExecuteStoredProc("KFspRemoveContsestantFromEvent", "@fname", fname, "@lname", lname, "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public void KFspUpdateContestant(string fname, string lname, int perid)
+    {
+        databaseCon.ExecuteStoredProc("KFspUpdateContestant", "@fname", fname, "@lname", lname, "@perid", perid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public DataTable KFspViewEventContestants(int eventid)
+    {
+        databaseCon.ExecuteStoredProc("KFspViewEventContestants", "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable KFspViewNotEventContestants(int eventid)
+    {
+        databaseCon.ExecuteStoredProc("KFspViewNotEventContestants", "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspGetEventCriteriaID(string Name, int EventJudgeID)
+    {
+        databaseCon.ExecuteStoredProc("MCspGetEventCriteriaID", "@Name", Name, "@EventJudgeID");
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public void MCspUpdateScore(int EventJudgeID, int ContestantID, int EventCriteriaID, float Score)
+    {
+        databaseCon.ExecuteStoredProc("MCspUpdateScore", "@EventJudgeID", EventJudgeID, "@ContestantID", ContestantID, "@EventCriteriaID", EventCriteriaID, "@Score", Score);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public DataTable MCspViewContestant(int ContestantID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewContestant", "@ContestantID", ContestantID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewContestants(string EventName)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewContestants", "@EventName", EventName);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewContestantsEvent(int EventID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewContestantsEvent", "@EventID", EventID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewCriteria(int EventJudgeID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewCriteria", "@EventJudgeID", EventJudgeID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewJudgeEvent(string EventName, int JID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewJudgeEvent", "@EventName", EventName, "@JID", JID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewJudges(int JudgeID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewJudges", "@JudgeID", JudgeID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewOfficialResults(int EventID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewOfficialResults", "@EventID", EventID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewScore(int EventJudgeID, int ContestantID, int EventCriteriaID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewScore", "@EventJudgeID", EventJudgeID, "@ContestantID", ContestantID, "@EventCriteriaID", EventCriteriaID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewScoreWeight(int ContestantID, int EventJudgeID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewScoreWeight", "@ContestantID", ContestantID, "@EventJudgeID", EventJudgeID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable MCspViewStatus(string EventName, int JudgeID, int ContestantID)
+    {
+        databaseCon.ExecuteStoredProc("MCspViewStatus", "@EventName", EventName, "@JudgeID", JudgeID, "@ContestantID", ContestantID);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public void spAddCriteriaToEventCriteria(string cname, int weight, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spAddCriteriaToEventCriteria", "@cname", cname, "@weight", weight, "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public void spAddJudgeToEventJudges(string fname, string lname, string uname, string pass, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spAddJudgeToEventJudges", "@fname", fname, "@lname", lname, "@uname", uname, "@pass", pass, "@eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public void spAddOldCriteriaToEventCriteria(string cname, int weight, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spAddOldCriteriaToEventCriteria", "@cname", cname, "@weight", weight, "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public void spAddPersonToEventJudges(string fname, string lname, string uname, string pass, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spAddPersonToEventJudges", "@fname", fname, "@lname", lname, "@uname", uname, "@pass", pass, "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public DataTable spCheckCExistance(string cname, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spCheckCExistance", "@cname", cname, "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable spCheckcnameavailability(string cname)
+    {
+        databaseCon.ExecuteStoredProc("spCheckcnameavailability", "@cname", cname);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable spCheckPersonExistanceinEvent(string fname, string lname, string uname, string pass, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spCheckPersonExistanceinEvent", "@fname", fname, "@lname", lname, "@uname", uname, "@pass", pass, "@eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable spCheckUnameavailability(string judgechars)
+    {
+        databaseCon.ExecuteStoredProc("spCheckUnameavailability", "@judgechars", judgechars);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public void spRemoveCriteriaFromEventCriteria(string cname, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spRemoveCriteriaFromEventCriteria", "@cname", cname, "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public void spRemoveJudgefromEventJudges(string fname, string lname, string uname, string pass)
+    {
+        databaseCon.ExecuteStoredProc("spRemoveJudgefromEventJudges", "@fname", fname, "@lname", lname, "@uname", uname, "@pass", pass);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public void spUpdateEventCriteria(string cname, int weight, int eventid, int critid)
+    {
+        databaseCon.ExecuteStoredProc("spUpdateEventCriteria", "@cname", cname, "@weight", weight, "@_eventid", eventid, "@critid", critid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public void spUpdatePersonJudgeDetails(int personid, string fname, string lname, string uname, string pass, int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spUpdatePersonJudgeDetails", "@personid", personid, "@fname", fname, "@lname", lname, "@uname", uname, "@pass", pass, "@eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+    }
+    public DataTable spViewEventCriteria(int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spViewEventCriteria", "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable spViewEventJudges(int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spViewEventJudges", "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable spViewNotEventCriteria(int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spViewNotEventCriteria", "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
+    public DataTable spViewNotEventJudges(int eventid)
+    {
+        databaseCon.ExecuteStoredProc("spViewNotEventJudges", "@_eventid", eventid);
+        hasError = databaseCon.HasError;
+        error = databaseCon.Error;
+        return databaseCon.Data;
+    }
 }
+
