ae53bd44530383df572fbef0c9577a63021b2573
firenzy <lf.oripaypay@gmail.com>
2018-04-10 16:14:53 +0800

    Added Database.cs to WCF

--- WCF/App_Code/Service.cs
+++ WCF/App_Code/Service.cs
@@ -8,6 +8,8 @@ using System.Text;
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
 public class Service : IService
 {
+    Database dbObject = new Database();
+
     public void DoWork()
     {
     }
