b56e93f88ef60783cfde7dcb97e6d7dcc16cead7
Christopher <christophermb@outlook.com>
2018-04-20 21:17:38 +0800

    test contestantsTab

---
+++ APACHE/APACHE/www/scripts/contestantscript.js
@@ -0,0 +1,12 @@
+﻿var selected; 
+
+$("#tbAddContestant tbody tr").click(function () {
+    $(this).addClass('selectedContestant').siblings().removeClass('selectedContestant');
+    selected = $(this).find('td:first').html() + " " + $(this).find('td:nth-child(2)').html();
+    //alert(selected); 
+});
+
+function clickAddContestant() {
+    alert(selected);
+
+}
