e9d7a1d3d2f7e56d5b35f14b58488aaa3499ad46
firenzy <lr.oripaypay@gmail.com>
2018-05-04 22:47:14 +0800

    CriteriaTab v1.0 All major functionalities done

---
+++ APACHE/APACHE/www/scripts/judgescript.js
@@ -0,0 +1,17 @@
+﻿var selected, modify, input, fName, lName, action;
+
+
+//selects from slAddContestant
+$("#tbAddJudges tbody tr").click(function () {
+
+    //gets data
+    //selected = $("#tbAddJudges tbody td").val();
+    //input = selected.split(" ");
+    //fName = input[0];
+    //lName = input[1];
+
+    //alert(selected);
+
+    document.getElementById("btnAddExistJudge").disabled = false;
+
+});
