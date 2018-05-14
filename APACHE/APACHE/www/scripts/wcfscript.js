dc4481baa469408c7629b29cce6668a11dd6574e
Christian Kyle Floresta <ckdfloresta@mymail.mapua.edu.ph>
2018-05-14 16:56:11 +0800

    fixed home html redirecting to create event

--- APACHE/APACHE/www/scripts/wcfscript.js
+++ APACHE/APACHE/www/scripts/wcfscript.js
@@ -4,8 +4,8 @@
 
 var ipAddress = 'localhost'; //Use this if you need to change the ip address
 var directory = 'UVSApp-WCF'; //Use this to change your WCF directory name (change to UVSApp-WCF to make life easier) 
-var service = 'http://' + ipAddress + '/' + directory + '/service.svc/'; 
-
+//var service = 'http://' + ipAddress + '/' + directory + '/service.svc/'; 
+var service = 'http://localhost/service.svc/';
 
 //index.html
 function btnLogin_OnClick() {
